d9ada43b766b94f32e4276936fb5ae3ef6a804a1
Tony Manion <tonymanion@live.com>
2023-05-22 16:57:25 -0500

    more cleanup

--- src/main/java/com/example/demo/model/Receipt.java
+++ src/main/java/com/example/demo/model/Receipt.java
@@ -5,9 +5,6 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
